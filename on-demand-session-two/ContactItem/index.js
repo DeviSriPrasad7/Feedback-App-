a877de6adfc9b8e1a819fe9f21d3b8c6ac5284bc
DeviSriPrasad7 <112795836+DeviSriPrasad7@users.noreply.github.com>
2022-11-06 09:39:31 +0530

    Create index.js

---
+++ on-demand-session-two/ContactItem/index.js
@@ -0,0 +1,36 @@
+import './index.css'
+
+const ContactItem = props => {
+  const {contactDetails, toggleIsFavorite} = props
+  const {name, mobileNo, isFavorite, id} = contactDetails
+
+  const starImgUrl = isFavorite
+    ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
+
+  const favoriteItem = () => {
+    toggleIsFavorite(id)
+  }
+
+  return (
+    <li className="table-row">
+      <div className="table-cell name-column">
+        <p>{name}</p>
+      </div>
+      <hr className="separator" />
+      <div className="table-cell mobile-no-column">
+        <p className="mobile-no-value">{mobileNo}</p>
+        <button type="button" className="favorite-icon-container">
+          <img
+            src={starImgUrl}
+            className="favorite-icon"
+            alt="star"
+            onClick={favoriteItem}
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default ContactItem
